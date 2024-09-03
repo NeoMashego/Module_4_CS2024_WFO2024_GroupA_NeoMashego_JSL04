9dcae3bb64fbd104032e4b57436901f4c059d3fc
NeoMashego <mashegonoj@gmail.com>
2024-09-03 13:36:15 +0200

    if statement created to check userPreference and console.log results

--- index.js
+++ index.js
@@ -4,8 +4,13 @@
 // Given a variable that holds a user's preferred type of Monster Energy drink, 
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
-let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+let userPreference = ''; // Possible values: 'Regular', 'Sugar-free', or undefined
+if (userPreference == ''){
+   console.log('Regular');
+} else {
+    console.log('Sugar-Free');
+}
+console.log(prefer);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 

34cec06d3bcac73f802b833c28fc4b8de81a27c9
NeoMashego <mashegonoj@gmail.com>
2024-09-04 08:50:32 +0200

    Created If statement to look for currentHour

--- index.js
+++ index.js
@@ -37,11 +37,18 @@ console.log(cold);
 
 // #### Challenge 5: Late Night Coding Session
 // Determine if it's a good idea to have a Monster Energy drink based on the current hour.
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!",
+// If it's between 7am and midnight (24-hour format), print "Unleash the beast!", 07:00am - 00:00am 
 // otherwise, print "Better stick to water."
 
 // Define the current hour in 24-hour format
-let currentHour = 22; 
+let currentHour = 23;   
+if (currentHour > 7 && currentHour > 24){
+    console.log("Unleash the beast!");
+} else {
+    console.log("Better stick to water.");
+}
+let drinkTime;
+console.log();
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
